be9eece303ddc54b2fac52aa15fa31bf29ae1e11
setiyoadiwicaksono@yahoo.com <asbun1996>
2017-03-01 17:49:32 +0700

    halaman awal prediksi keuntungan

---
+++ LokataniApps/app/src/main/java/com/example/nindyasaridu/lokataniapps/PrediksiKeuntunganActivity.java
@@ -0,0 +1,13 @@
+package com.example.nindyasaridu.lokataniapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrediksiKeuntunganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prediksi_keuntungan);
+    }
+}
