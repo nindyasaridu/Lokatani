5e90d95d629c0d2fa6ca48595cfa172adb3bdc7c
setiyoadiwicaksono@yahoo.com <asbun1996>
2017-03-01 18:08:38 +0700

    ni java yang hilang

---
+++ LokataniApps/app/src/main/java/com/example/nindyasaridu/lokataniapps/KelolaLahanActivity.java
@@ -0,0 +1,15 @@
+package com.example.nindyasaridu.lokataniapps;
+
+import android.os.Bundle;
+
+/**
+ * Created by Setiyo on 3/1/2017.
+ */
+
+public class KelolaLahanActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelola_lahan);
+    }
+}
