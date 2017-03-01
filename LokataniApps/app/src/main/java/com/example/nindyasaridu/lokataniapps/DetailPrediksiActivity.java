8c5fe9e1baf6fd6f1ea427bc1f1c5581d491e974
setiyoadiwicaksono@yahoo.com <asbun1996>
2017-03-01 18:53:39 +0700

    detail prediksi

---
+++ LokataniApps/app/src/main/java/com/example/nindyasaridu/lokataniapps/DetailPrediksiActivity.java
@@ -0,0 +1,13 @@
+package com.example.nindyasaridu.lokataniapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailPrediksiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_prediksi);
+    }
+}
