dacf83e2e5198f9dc52390899237cd5beea7c2b9
sdarmaputra <surya.igede@gmail.com>
2017-03-10 06:21:55 +0700

    fitur prediksit keuntungan selesai

--- LokataniApps/app/src/main/java/com/example/nindyasaridu/lokataniapps/DetilPrediksiActivity.java
+++ LokataniApps/app/src/main/java/com/example/nindyasaridu/lokataniapps/DetilPrediksiActivity.java
@@ -1,7 +1,19 @@
 package com.example.nindyasaridu.lokataniapps;
 
+import android.content.Intent;
+import android.renderscript.Double2;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class DetilPrediksiActivity extends AppCompatActivity {
 
@@ -9,5 +21,57 @@ public class DetilPrediksiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detil_prediksi);
+
+        LinearLayout detilPrediksiWrapper = (LinearLayout) findViewById(R.id.detilPrediksiWrapper);
+
+        Intent intent = getIntent();
+        Double totalPengeluaran = intent.getDoubleExtra("totalPengeluaran", 0.0);
+        Double jumlahHasilPanen = intent.getDoubleExtra("jumlahHasilPanen", 0.0);
+        Double rencanaHarga = intent.getDoubleExtra("rencanaHarga", 0.0);
+
+        Double hasilStart = jumlahHasilPanen - (jumlahHasilPanen / 2);
+        Double hasilEnd = jumlahHasilPanen + (jumlahHasilPanen / 2);
+        Double intervalHasil = ( jumlahHasilPanen - hasilStart ) / 2;
+
+        Double currentHasil = hasilStart;
+        while (currentHasil <= hasilEnd ) {
+            Double hargaJual = currentHasil * rencanaHarga;
+            Double keuntungan = hargaJual - totalPengeluaran;
+
+            LinearLayout itemPrediksiWrapper = new LinearLayout(this);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp.weight = 1;
+            lp.bottomMargin = 20;
+            itemPrediksiWrapper.setBackground(getDrawable(R.drawable.card));
+            itemPrediksiWrapper.setPadding(50, 80, 50, 80);
+            itemPrediksiWrapper.setLayoutParams(lp);
+
+            LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp2.weight = 1;
+            lp2.gravity = Gravity.CENTER_VERTICAL;
+            TextView terjual = new TextView(this);
+            terjual.setText("Terjual " + currentHasil + " Kg");
+            terjual.setLayoutParams(lp2);
+            terjual.setTextSize(16);
+            TextView untung = new TextView(this);
+            String keuntunganString = (String) NumberFormat.getNumberInstance(Locale.US).format(keuntungan.intValue());
+            untung.setText("Rp." + keuntunganString);
+            untung.setLayoutParams(lp2);
+            untung.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            untung.setTextSize(20);
+            if (keuntungan.intValue() > 0) {
+                untung.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
+            } else if (keuntungan.intValue() < 0) {
+                untung.setTextColor(ContextCompat.getColor(this, R.color.colorRedText));
+            } else {
+                untung.setTextColor(ContextCompat.getColor(this, R.color.colorSecondaryText));
+            }
+
+            itemPrediksiWrapper.addView(terjual);
+            itemPrediksiWrapper.addView(untung);
+            detilPrediksiWrapper.addView(itemPrediksiWrapper);
+
+            currentHasil += intervalHasil;
+        }
     }
 }
--- LokataniApps/app/src/main/java/com/example/nindyasaridu/lokataniapps/PrediksiKeuntunganActivity.java
+++ LokataniApps/app/src/main/java/com/example/nindyasaridu/lokataniapps/PrediksiKeuntunganActivity.java
@@ -1,34 +1,165 @@
 package com.example.nindyasaridu.lokataniapps;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpRetryException;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 public class PrediksiKeuntunganActivity extends AppCompatActivity {
     private Button btnPrediksi;
+
+    private ScrollView scrollWrapper = null;
+    private LinearLayout detailPengeluaranLayoutWrapper = null;
+    private LinearLayout bahanWrapper = null;
+    private LinearLayout hargaWrapper = null;
+    private AlertDialog detailPengeluaranDialog = null;
+    private Integer pengeluaran = 0;
+
+    protected String pengeluaranUrl = "http://128.199.127.175/lokatani_db/getTransaksiLahanByJenis.php?id_lahan=1&&jenis_transaksi=-1";
+    protected Connection conn = new Connection(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prediksi_keuntungan);
 
+        if (conn.connectionCheck()) {
+            String res;
+            try {
+                res = new HttpTask(this).execute(this.pengeluaranUrl).get();
+                JSONObject response = new JSONObject(res);
+                JSONArray transaksi_lahan  = response.getJSONArray("transaksi_lahan");
+
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT);
+                lp.weight = 1;
+
+                scrollWrapper = new ScrollView(this);
+                scrollWrapper.setPadding(0, 80, 0, 80);
+                scrollWrapper.setOverScrollMode(View.OVER_SCROLL_NEVER);
+                detailPengeluaranLayoutWrapper = new LinearLayout(this);
+                detailPengeluaranLayoutWrapper.setPadding(50, 20, 50, 20);
+                scrollWrapper.addView(detailPengeluaranLayoutWrapper);
+                bahanWrapper = new LinearLayout(this);
+                hargaWrapper = new LinearLayout(this);
+
+                bahanWrapper.setOrientation(LinearLayout.VERTICAL);
+                bahanWrapper.setLayoutParams(lp);
+                hargaWrapper.setOrientation(LinearLayout.VERTICAL);
+                hargaWrapper.setLayoutParams(lp);
+                detailPengeluaranLayoutWrapper.addView(bahanWrapper);
+                detailPengeluaranLayoutWrapper.addView(hargaWrapper);
+
+                for(int i=0; i<transaksi_lahan.length(); i++){
+                    JSONObject transaksi = (JSONObject) transaksi_lahan.get(i);
+                    Integer jumlah_transaksi = Integer.parseInt(transaksi.getString("jumlah_transaksi"));
+                    pengeluaran += jumlah_transaksi;
+
+                        TextView bahan = new TextView(this);
+                        bahan.setText(transaksi.getString("nama_transaksi"));
+                        bahanWrapper.addView(bahan);
+
+                        TextView harga = new TextView(this);
+                        String harga_string = (String) NumberFormat.getNumberInstance(Locale.US).format(jumlah_transaksi);
+                        harga.setText("Rp." + harga_string);
+                        harga.setTextColor(getResources().getInteger(R.color.colorAccent));
+                        harga.setGravity(Gravity.RIGHT);
+                        hargaWrapper.addView(harga);
+
+                }
+
+                TextView totalPengeluaranTv = (TextView) findViewById(R.id.tvTotalPengeluaran);
+                String totalPengeluaran = (String) NumberFormat.getNumberInstance(Locale.US).format(pengeluaran);
+                totalPengeluaranTv.setText("Rp." + totalPengeluaran);
+
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+                alertBuilder.setView(this.scrollWrapper);
+                alertBuilder.setNegativeButton("Tutup", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+                detailPengeluaranDialog = alertBuilder.create();
+                detailPengeluaranDialog.setOnShowListener( new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface arg0) {
+                        Button negativeButton = detailPengeluaranDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+                        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                        lp.weight = 2;
+                        lp.bottomMargin = 10;
+                        lp.topMargin = 10;
+                        negativeButton.setBackground(getDrawable(R.drawable.rounded_button));
+                        negativeButton.setTextColor(Color.parseColor("#FEFEFE"));
+                        negativeButton.setLayoutParams(lp);
+                    }
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            conn.failureAlert();
+        }
+
+        TextView judulDetailPengeluaranTv = (TextView) findViewById(R.id.tvJudulDetailPengeluaran);
+        judulDetailPengeluaranTv.setOnClickListener(operasi);
+
         btnPrediksi     = (Button) findViewById(R.id.btnPrediksi);
         btnPrediksi.setOnClickListener(operasi);
     }
+
     View.OnClickListener operasi = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()){
-                case R.id.btnPrediksi:Register(); break;
+                case R.id.btnPrediksi:
+                    openTambahIntent();
+                    break;
+                case R.id.tvJudulDetailPengeluaran:
+                    openDetailPengeluaranWrapper();
+                    break;
             }
         }
     };
 
-    private void Register(){
-        Intent intentTambah = new Intent(getBaseContext(), DetilPrediksiActivity.class);
-        startActivityForResult(intentTambah, 0);
+    private void openTambahIntent(){
+        Intent intentDetilPrediksi = new Intent(getBaseContext(), DetilPrediksiActivity.class);
+        EditText textJumlahHasilPanen = (EditText) findViewById(R.id.textJumlahHasilPanen);
+        EditText textRencanaHarga = (EditText) findViewById(R.id.textRencanaHarga); 
+        Double jumlahHasilPanen = Double.parseDouble(textJumlahHasilPanen.getText().toString());
+        Double rencanaHarga = Double.parseDouble(textRencanaHarga.getText().toString());
+        intentDetilPrediksi.putExtra("totalPengeluaran", Double.parseDouble(this.pengeluaran.toString()));
+        intentDetilPrediksi.putExtra("jumlahHasilPanen", jumlahHasilPanen);
+        intentDetilPrediksi.putExtra("rencanaHarga", rencanaHarga);
+        startActivityForResult(intentDetilPrediksi, 0);
+    }
+
+    private void openDetailPengeluaranWrapper() {
+        detailPengeluaranDialog.show();
     }
 
 }
